a3f3c0bfbe14c66ea3697cdd4b584d9c9b8be111
junior <mkgpul005@myuct.ac.za>
2019-08-01 21:50:16 +0100

    commented final working project

--- prac1.py
+++ prac1.py
@@ -10,41 +10,46 @@ Date: 26/07/2019
 """
 
 # import Relevant Librares
-import RPi.GPIO as GPIO
-from time import sleep
-from itertools import product
+import RPi.GPIO as GPIO				#imports python libraries
+from time import sleep				#import sleep module to be used for delays
+from itertools import product			#imoprts itertools module product
 
-def countup(channel):
-	global counter
+def countup(channel):				#defines a function that will increment a counter value
+	global counter				 #when interrupt is detected
 	print("counting up")
-	if (counter ==7):
-		counter =0
+	if (counter ==7):			#checks if the final value in the array has been reached,
+		counter =0			 #if so the counter variable is reset to zero
 	else:
 		counter=counter+1
-def countdown(channel):
+def countdown(channel):				#defines a function that will decrement a counter value
 	global counter
 	print("counting down")
 	if (counter == 0):
-		counter =7
-	else:
+		counter =7			#checks if the first value in the array has been reached,
+	else:					 #if so, it goes back to the highest number in array
 		counter=counter-1
 
-GPIO.setmode(GPIO.BOARD)
-channels = (11,13,15)
+GPIO.setmode(GPIO.BOARD)			#indicates that pin numbers are used
+channels = (11,13,15)				#channels to be initialized
 
-GPIO.setup(channels,GPIO.OUT)
+GPIO.setup(channels,GPIO.OUT)			#sets channels to output pins
+
+#sets pin 31 as an input and initalizes the pull down resistors
 GPIO.setup(31,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
+#creates interrupt when switch is pressed and calls functions
 GPIO.add_event_detect(31,GPIO.RISING, callback=countup,bouncetime=500)
 GPIO.add_event_detect(12,GPIO.RISING, callback=countdown,bouncetime=500)
 
-# Logic that you write
+# generates an array of binary values from [0,0,0] to [1,1,1]
+#defines a counter variable
 lst =list(product([0,1],repeat=3))
 counter=0
 
 def main():
 
+	#sends binary array values to be output on the LEDs and delays for 0.5 seconds
 	GPIO.output(channels,lst[counter])
 	sleep(0.5)
 
